c846f8b784ae91d78028a70fb20a7b0557c3cade
Arifozden <111346692+Arifozden@users.noreply.github.com>
2023-05-20 20:22:53 +0200

    Initial commit

---
+++ src/main/java/otopark/otoparkoturumacma/OtoparkOturumAcmaApplication.java
@@ -0,0 +1,13 @@
+package otopark.otoparkoturumacma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtoparkOturumAcmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtoparkOturumAcmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/otopark/otoparkoturumacma/OtoparkOturumAcmaApplicationTests.java
@@ -0,0 +1,13 @@
+package otopark.otoparkoturumacma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtoparkOturumAcmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
